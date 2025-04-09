ca4a826d463e12e4330de16add834d2b20184fcd
Anoshka Jhaveri <anoshkaujhaveri@gmail.com>
2025-04-09 03:45:47 -0700

    updated color dropdown

---
+++ src/pages/AutoRiggerPage/AutoRiggerPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function AutoRiggerPage() {
+  return (
+    <div>
+      A powerful automation tool designed to streamline the character rigging
+      process. This tool significantly reduces the manual effort required,
+      providing a fast, efficient, and consistent rigging workflow. - Fully
+      automated joint placement and skinning process - Supports both biped and
+      quadruped characters - Intuitive UI for custom adjustments and fine-tuning
+      - Reduces rigging time by up to 70%{" "}
+    </div>
+  );
+}
+
+export default AutoRiggerPage;
